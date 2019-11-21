e27f8d15b08847378d1e9d2e286668d85785a456
Mix Abcprintf <wh.cprintf@gmail.com>
2019-11-21 11:23:14 +0700

    update runJob

--- CRON-JOB-URL/Main.cs
+++ CRON-JOB-URL/Main.cs
@@ -1,21 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using CRON_JOB_URL.CronNET;
+using System;
 using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
+using System.Web.Script.Serialization;
 using System.Windows.Forms;
 
 namespace CRON_JOB_URL
 {
     public partial class Main : Form
     {
+        private static readonly HttpClient client = new HttpClient();
+
+        private readonly CronDaemon cron_daemon = new CronDaemon();
+
         public Main()
         {
             InitializeComponent();
+
+            // get version
+            getVersion();
+
+            // init cronJob
+            cronJob();
         }
 
         public void getVersion()
@@ -23,7 +29,31 @@ namespace CRON_JOB_URL
             System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
             FileVersionInfo fileInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
 
-            lb_version.Text = fileInfo.FileVersion;
+            lb_version.Text = "Version " + fileInfo.FileVersion;
+        }
+
+        public async void runJob()
+        {
+            string urlBase = Properties.Settings.Default.BaseUrl;
+            var responseString = await client.GetStringAsync(urlBase);
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            dynamic item = serializer.Deserialize<object>(responseString);
+            string message = item["message"];
+            string datetime = item["dateTime"];
+
+            string textMessage = datetime + " - " + message;
+
+            tb_log.Invoke(new Action(() => tb_log.Text += textMessage.ToString() + Environment.NewLine));
+        }
+
+        public void cronJob()
+        {
+            var d = new CronDaemon();
+            // get CronJob from setting.
+            var cronschedule = Properties.Settings.Default.CronJob;
+            d.AddJob(cronschedule, () => { runJob(); });
+            d.Start();
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
--- CRON-JOB-URL/Properties/Settings.Designer.cs
+++ CRON-JOB-URL/Properties/Settings.Designer.cs
@@ -8,22 +8,36 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CRON_JOB_URL.Properties
-{
-
-
+namespace CRON_JOB_URL.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://office.igenco.co.th/leave_igenco/validate-notify-api")]
+        public string BaseUrl {
+            get {
+                return ((string)(this["BaseUrl"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("* * * * *")]
+        public string CronJob {
+            get {
+                return ((string)(this["CronJob"]));
             }
         }
     }
