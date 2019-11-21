6a2bb3ca0aefd96e7be247802f6564eab39bb17b
Mix Abcprintf <wh.cprintf@gmail.com>
2019-11-21 11:24:29 +0700

    update

--- CRON-JOB-URL/Properties/Settings.Designer.cs
+++ CRON-JOB-URL/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace CRON_JOB_URL.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://office.igenco.co.th/leave_igenco/validate-notify-api")]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://localhost/api/timestamp")]
         public string BaseUrl {
             get {
                 return ((string)(this["BaseUrl"]));
